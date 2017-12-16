76964896d77b151a510b712d666cce36d2e5caab
MaxIv2 <ivmaxxim@gmail.com>
2017-12-16 13:22:46 +0200

    Added Hello World in MouseServer.cs

--- QRCodeConnect/QRCodeConnect/MouseServer.cs
+++ QRCodeConnect/QRCodeConnect/MouseServer.cs
@@ -5,14 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
+using System.Threading;
 
 
 namespace QRCodeConnect {
     class MouseServer {
         private int port;
         private string ip;
+        private Socket serverSocket;
+
         public MouseServer() {
-            Socket serverSocket = new Socket(SocketType.Dgram, ProtocolType.IP);
+            this.serverSocket = new Socket(SocketType.Dgram, ProtocolType.IP);
             this.ip = GetLocalIPAddress();
             string[] splitIP = ip.Split('.');
             byte[] address = new byte[4];
@@ -20,9 +23,12 @@ namespace QRCodeConnect {
                 address[i] = byte.Parse(splitIP[i]);
             }
             IPEndPoint localEP = new IPEndPoint(new IPAddress(address), 0);
-            serverSocket.Bind(localEP);
+            this.serverSocket.Bind(localEP);
             this.port = (serverSocket.LocalEndPoint as IPEndPoint).Port;
+            Thread serverThread = new Thread(ServerThread);
+            serverThread.Start();
         }
+
         public static string GetLocalIPAddress() {
             var host = Dns.GetHostEntry(Dns.GetHostName());
             foreach (var ip in host.AddressList) {
@@ -32,11 +38,26 @@ namespace QRCodeConnect {
             }
             throw new Exception("No network adapters with an IPv4 address in the system!");
         }
+
         public int GetPort() {
             return this.port;
         }
+
         public string GetIP() {
             return this.ip;
+        }
+
+        public Socket GetServerSocket() {
+            return this.serverSocket;
+        }
+
+        public void ServerThread() {
+            this.serverSocket.Listen(1);
+            Socket clientSide = this.serverSocket.Accept();
+            byte[] buffer = Encoding.ASCII.GetBytes("Hello World!");
+            clientSide.Send(buffer);
+            clientSide.Close();
+            this.serverSocket.Close();
         }
     }
 }
