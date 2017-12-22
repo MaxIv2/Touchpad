e90be166e8d18901ccade5ca7116a934618b1458
MaxIv2 <ivmaxxim@gmail.com>
2017-12-22 22:03:06 +0200

    Technical difficulties... nothing to see here

---
+++ TouchpadServer/TouchpadController/SystemTrayIcon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TouchpadController {
+    class SystemTrayIcon {
+        private NotifyIcon trayIcon;
+
+        public SystemTrayIcon() {
+            trayIcon = new NotifyIcon();
+            trayIcon.Text = "Remote touchpad settings";
+            //trayIcon.Icon = TouchpadController.Proper
+            trayIcon.Visible = true;
+            trayIcon.Click += OnIconClick;
+            trayIcon.DoubleClick += this.OnDoubleClick;
+        }
+
+
+        public void OnDoubleClick(object sender, EventArgs e) {
+            //this.Close();
+        }
+
+        private void SetVisibilty(bool visible) {
+            //this.Visible = visible;
+            //this.ShowInTaskbar = visible;
+        }
+
+        public void OnIconClick(object sender, EventArgs e) {
+            //SetVisibilty(!this.Visible);
+        }
+    }
+}
