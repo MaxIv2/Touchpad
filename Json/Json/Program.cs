b9157ca307ee6515c836083eafcc0edf5ed3234e
MaxIv2 <ivmaxxim@gmail.com>
2018-01-11 18:46:56 +0200

    added a seperate project for the http client that was recently added on the movestream branch

---
+++ Json/Json/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Json {
+
+    class Program {
+        static void Main(string[] args) {
+            // if you use https the server port must be 443 and if you use http than the port 80
+            // otherwise the code will throw an exception
+            DiagnosticsReporter reporter = new DiagnosticsReporter("http://localhost:80");
+            Console.WriteLine("First");
+            System.Threading.Thread.Sleep(5000);
+        }
+    }
+}
+
